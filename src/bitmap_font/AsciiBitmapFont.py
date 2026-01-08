ee3df60ebeb7f0e3642c5506a28f02b38b749146
Sergey Semushin <kezzyhko@gmail.com>
2026-01-08 16:42:09 +0300

    Change font structure a bit

--- src/bitmap_font/AsciiBitmapFont.py
+++ src/bitmap_font/AsciiBitmapFont.py
@@ -6,29 +6,35 @@ from pathlib import Path
 
 class AsciiBitmapFont(ImageFont):
 
+	DEFAULT_SPACING = 1
+
 	@staticmethod
 	def get_default_font(size: int) -> AsciiBitmapFont:
 		path = Path(__file__).parent / "default.font.toml"
 		return AsciiBitmapFont(path, size)
 
-	def __init__(self, path: str | Path, size: int):
+	def __init__(self, path: str | Path, size: int, spacing: int | None = None):
 		super().__init__()
 		self.path = path
 		self.size = size
+		self.spacing = spacing
 		self.glyphs = {}
 		self._load_file()
 
 	def _load_file(self):
 		with open(self.path, "rb") as f:
 			font_data = tomllib.load(f)
+		
+		configuration = font_data.get('configuration', {})
+		self.spacing = self.spacing or configuration.get('default_spacing') or AsciiBitmapFont.DEFAULT_SPACING
 
 		for glyph in font_data['glyphs']:
 			bitmap = glyph['bitmap'].strip("\n").split("\n")
 			size = len(bitmap)
 			if (size != self.size):
 				continue # TODO: load and cache all file as a collection of fonts with different sizes
-			for letter in glyph['letters']:
-				self.glyphs[letter] = bitmap
+			for character in glyph['characters']:
+				self.glyphs[character] = bitmap
 
 	def getmask(self, text: str) -> Image.core.ImagingCore:
 		w = self.width * len(text)
