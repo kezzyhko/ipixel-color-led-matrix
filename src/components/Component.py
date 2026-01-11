41edfacaea1342f7aacf1b96c173213d8510c62d
Sergey Semushin <kezzyhko@gmail.com>
2026-01-12 01:31:18 +0300

    Add ability to set Component's parent directly

--- src/components/Component.py
+++ src/components/Component.py
@@ -1,12 +1,32 @@
 from abc import ABCMeta, abstractmethod
 from PIL.Image import Image
+from . import Group
 
 
 class Component(metaclass=ABCMeta):
 	def __init__(self):
 		self.x = 0
 		self.y = 0
-		self.parent: Component|None = None
+		self._parent: Group|None = None
+
+	@property
+	def parent(self) -> Group|None:
+		return self._parent
+
+	@parent.setter
+	def parent(self, new_parent: Group|None):
+		del self.parent
+		if not new_parent:
+			return
+		self._parent = new_parent
+		new_parent.add_child(self)
+
+	@parent.deleter
+	def parent(self):
+		if not self._parent:
+			return
+		self._parent.remove_child(self)
+		self._parent = None
 
 	@abstractmethod
 	def update(self):
