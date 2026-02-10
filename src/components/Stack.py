b072041c14c6580425ebe87357591c91a63fba0a
kezzyhko <kezzyhko@gmail.com>
2026-02-10 13:34:55 +0300

    Implement Pass 2 for Stack

--- src/components/Stack.py
+++ src/components/Stack.py
@@ -17,24 +17,38 @@ class Stack(Group):
 		main_axis, cross_axis = (0, 1) if self._direction == 'horizontal' else (1, 0)
 		stack_size = self._render_properties.max_size
 		
-		max_cross_size = 0
-		total_main_size = 0
+		max_cross_size: int = 0
+		main_size_left: int = stack_size[main_axis]
+		total_weight: float = 0.0
 
+		# Pass 1: render already defined sizes and get stats for pass 2
 		for child in self.children:
 			child_main_size = child.placement.size[main_axis]
 			child_cross_size = child.placement.size[cross_axis]
 			pixels_max_cross_size = round(child_cross_size * stack_size[cross_axis]) if child_cross_size is not None else stack_size[cross_axis]
 			pixels_max_main_size = round(child_main_size * stack_size[main_axis]) if child_main_size is not None else None			
-			child_max_size = (pixels_max_main_size, pixels_max_cross_size) if self._direction == 'horizontal' else (pixels_max_cross_size, pixels_max_main_size)
-
+			child_max_size = self._get_xy_from_maincross(pixels_max_main_size, pixels_max_cross_size)
 			child._render_properties.max_size = child_max_size
+
+			if child_main_size is None and child._render_properties.sizing_mode[main_axis] == 'input':
+				total_weight += child.placement.weight
+
 			if child._render_properties.is_max_size_ready:
 				child.render()
 				child_size = child._render_properties.size
-				total_main_size += child_size[main_axis]
+				main_size_left -= child_size[main_axis]
 				max_cross_size = max(max_cross_size, child_size[cross_axis])
 
+		# Pass 2: render rest of components by weight
 		for child in self.children:
+			if child._render_properties.max_size[main_axis] is not None:
+				continue
+			pixels_max_main_size = round(main_size_left * child.placement.weight / total_weight)
+			child._render_properties.max_size = self._get_xy_from_maincross(pixels_max_main_size, child._render_properties.max_size[cross_axis])
+			child.render()
+
+	def _get_xy_from_maincross(self, main: int|None, cross: int|None) -> tuple[int|None, int|None]:
+		return (main, cross) if self._direction == 'horizontal' else (cross, main)
 
 	def _render_implementation(self) -> Image.Image:
 		size = (self._render_properties.max_width, self._render_properties.max_height) # TODO: shrink if necessary
