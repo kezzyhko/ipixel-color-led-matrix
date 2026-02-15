f3281300addd167592ff1ae632340afda38e8575
kezzyhko <kezzyhko@gmail.com>
2026-02-15 15:31:24 +0300

    Improve icon name selection for clear sky (sun/moon)

--- src/components/WeatherComponent.py
+++ src/components/WeatherComponent.py
@@ -43,26 +43,22 @@ class WeatherComponent(Stack):
 	def _get_icon_name(self, weather: CurrentWeather) -> str:
 		weather_type_info = WeatherTypeInfo.from_code(weather.weathercode)
 
-		# Special case for clear sky - depends on more stuff
-		base_icon_name = weather_type_info.type
-		if base_icon_name == 'clear':
-			hour = datetime.fromisoformat(weather.time).hour # TODO: Use time for actual sunrise/sunset
-			is_night_time = hour < 6 or hour >= 19
-			base_icon_name = "moon" if is_night_time else "sun" #TODO: Include lunar phase? Detect rotation of moon based on location??
+		# Determine celestial body (sun/moon)
+		hour = datetime.fromisoformat(weather.time).hour # TODO: Use time for actual sunrise/sunset
+		is_night_time = hour < 6 or hour >= 19
+		celestial_body += 'moon' if is_night_time else 'sun' #TODO: Include lunar phase? Detect rotation of moon based on location??
 		
 		# Choose power name
 		match weather_type_info.power:
 			case p if p > 0.66:
-				power_name = "severe"
-			case p if p > 0.33:
-				power_name = "moderate"
-			case p if p > 0.00:
-				power_name = "light"
+				power_name = 'severe'
+			case p if p < 0.33:
+				power_name = 'light'
 			case _:
-				power_name = None
+				power_name = 'moderate'
 
 		# Choose and return icon name
-		icon_name = base_icon_name
-		if power_name:
-			icon_name += f"_{power_name}"
-		return icon_name
+		if weather_type_info.type == 'clear':
+			return f"{weather_type_info.type}_{celestial_body}"
+		else:
+			return f"{weather_type_info.type}_{power_name}"
